2da5c1f6b42afcfb74b77b66b84d2d0a490e3679
luciamcode <flwsilkysound@gmail.com>
2025-07-02 00:44:40 +0900

    Last Sync: 2025-07-02 00:44 (Mobile)

--- functions/create-gist.js
+++ functions/create-gist.js
@@ -1,8 +1,11 @@
+// functions/create-gist.js
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
   const { content } = JSON.parse(event.body);
+
+  // GitHub に Gist を作成
   const response = await fetch('https://api.github.com/gists', {
     method: 'POST',
     headers: {
@@ -18,9 +21,16 @@ exports.handler = async (event) => {
   if (!response.ok) {
     return { statusCode: response.status, body: 'GitHub API error' };
   }
+
   const data = await response.json();
+  // files オブジェクトの中身を取り出して raw_url を取得
+  const file = Object.values(data.files)[0];
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ html_url: data.html_url })
+    body: JSON.stringify({
+      html_url: data.html_url,
+      raw_url: file.raw_url
+    })
   };
 };
